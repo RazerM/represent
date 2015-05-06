466392be2783c4d1fe6cf3d5f6ed8a122e5d17e2
Frazer McLean <frazer@frazermclean.co.uk>
2015-05-07 01:30:09 +0200

    Add pprint to ReprMixin tests

--- represent/tests/test_mixin.py
+++ represent/tests/test_mixin.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division
 
 import pytest
+from IPython.lib.pretty import pprint, pretty
 
 from represent import ReprMixin
 
@@ -18,7 +19,10 @@ def test_standard():
             super(B, self).__init__()
 
     assert repr(A()) == 'A()'
+    assert pretty(A()) == 'A()'
+
     assert repr(B(1, 2)) == 'B(a=1, b=2, c=5)'
+    assert pretty(B(1, 2)) == 'B(a=1, b=2, c=5)'
 
 
 def test_positional():
@@ -51,9 +55,16 @@ def test_positional():
             super(D, self).__init__(positional=['a', 'b'])
 
     assert repr(A(1, 2)) == 'A(1, b=2, c=5)'
+    assert pretty(A(1, 2)) == 'A(1, b=2, c=5)'
+
     assert repr(B(1, 2)) == 'B(1, 2, c=5)'
+    assert pretty(B(1, 2)) == 'B(1, 2, c=5)'
+
     assert repr(C(1, 2)) == 'C(1, b=2, c=5)'
+    assert pretty(C(1, 2)) == 'C(1, b=2, c=5)'
+
     assert repr(D(1, 2)) == 'D(1, 2, c=5)'
+    assert pretty(D(1, 2)) == 'D(1, 2, c=5)'
 
     class E(ReprMixin, object):
         def __init__(self, a, b, c=5):
