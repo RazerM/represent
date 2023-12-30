6f9121c9c76e9b358bbe9c80e98d3aee13cd54fd
Frazer McLean <frazer@frazermclean.co.uk>
2023-12-30 19:02:53 +0000

    Migrate to src/ layout and pyproject.toml

--- represent/__init__.py
+++
@@ -1,9 +0,0 @@
-from .core import *
-from .helper import *
-
-__all__ = core.__all__ + helper.__all__
-
-__author__ = 'Frazer McLean <frazer@frazermclean.co.uk>'
-__version__ = '1.6.0.post0'
-__license__ = 'MIT'
-__description__ = 'Create __repr__ automatically or declaratively.'
